20125aea622a56051e3a4186b3b859bd7d868e96
Paul Smith <paul@pjsweb.uk>
2018-04-10 16:03:16 +0100

    Updated index to accommodate includes; CSS updated for front page search box

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php 
+include 'header.php';
+?>
 
-echo "Hello world!";
+		<!-- Banner -->
+		<section id="banner">
+			<div class="inner">
+				<h2>GoDMC Database</h2>
+				<p style="width: 50%; margin-left: auto; margin-right: auto">Genetics of DNA Methylation Consortium (GoDMC)</p>
+				<form action="/mqtldb/search-results.php" name="searchform" id="searchform" method="get">
+					<div class="input-group">
+		                <input type="text" class="form-control" placeholder="Search the GoDMC database" id="search" name="query">
+		                <span class="input-group-btn">
+		                    <button class="btn btn-default" type="submit" id="search-button">
+		                        <span class="glyphicon glyphicon-search"></span>
+		                    </button>
+		                </span>
+		            </div>
+		            <div class="examples">
+		                Examples:
+		                <a href="/mqtldb/search-results.php?query=rs7105015" data-original-title="" title="">rs7105015</a>,
+		                <a href="/mqtldb/search-results.php?query=cg24851651" data-original-title="" title="">cg24851651</a>,
+		                <a href="/mqtldb/search-results.php?query=6:16000000-25000000" data-original-title="" title="">6:16000000-25000000</a>
+		            </div>
+				</form>
+			</div>
+		</section>
 
+<?php 
+include 'footer.php';
 ?>

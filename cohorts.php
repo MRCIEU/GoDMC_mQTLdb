2a796524091944292d91c1e8405f5ca8d909a319
Paul Smith <paul@pjsweb.uk>
2018-07-09 11:41:51 +0100

    Added cohort content

--- cohorts.php
+++ cohorts.php
@@ -1,5 +1,12 @@
 <?php 
+
 include 'header.php';
+
+// Get JSON from API
+$url = 'http://api.godmc.org.uk/v0.1/cohorts';
+$content = file_get_contents($url);
+$data = json_decode($content,true);
+
 ?>
 
 <!-- Main -->
@@ -10,10 +17,41 @@ include 'header.php';
 				</header>
 				<div class="container">
 					<section>
-						
-            
 
+						<p>Select a cohort to see the details</p>
 
+					<?php
+
+            			if (count($data)) {
+					        // Open the list
+					        echo '<ul id="cohortlist">';
+
+					        // Cycle through the array
+					        foreach ($data as $cohort) {
+					        	echo '<li>';
+					        	echo '<a href="#'.$cohort['name'].'" onclick="toggle_visibility(\''.strtolower($cohort['name']).'\')"><h3 class="togglecohort">'.$cohort['name'].'</h3></a>';
+					        	echo '<div class="tablehide" id="'.strtolower($cohort['name']).'">';
+					        	echo '<table>';
+					        	foreach ($cohort as $key=>$value) {
+    
+						            if ($key == 'name') {
+					        			continue;
+					        		}
+					        		$title = str_replace('_', ' ', $key);
+						            echo '<tr><th>'.ucfirst($title).'</th><td>'.$value.'</td></tr>';					        
+						        }
+						        echo "</table>";
+						        echo '</div>';
+						        echo '</li>';
+					        }
+
+					        echo '</ul>';
+
+					        
+					        
+					    }
+
+					?>
 
 
 
@@ -26,3 +64,14 @@ include 'header.php';
 <?php 
 include 'footer.php';
 ?>
+<script type="text/javascript">
+<!--
+    function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+//-->
+</script>

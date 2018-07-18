cf6432c6c3853fd4a260c08de1c6694dd1e2f7d1
Paul Smith <paul@pjsweb.uk>
2018-07-18 12:59:15 +0100

    File load now dynamic; downloads relevant first line of BED file to get parameters

--- browser.php
+++ browser.php
@@ -1,13 +1,47 @@
 <?php 
 include 'header.php';
+ini_set("auto_detect_line_endings", true);
+
+$hosturi =  '//'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
+
 
 if (isset($_GET['cpg']) && $_GET['cpg'] != '') {
 	$query =  $_GET['cpg'];
-	$showquery = $query;
+	$cpg = $query;
 } else {
 	$query = '';
-	$showquery = 'No CPG provided';
+	$cpg = 'No CPG provided';
 }
+
+if ($query != '') {
+	$filenamebb = './data/'.$query.'.bb';
+}
+
+
+// Get BIGBED file if not already downloaded and write to filesystem
+if (file_exists($filenamebb)) {
+    //echo "The file $filenamebb exists";
+} else {
+    //echo "The file $filenamebb does not exist, downloading.";
+    $url = 'http://api.godmc.org.uk/v0.1/dl/bigbed/cpg/'.$query;
+	$file = basename($url);
+	$fp = fopen($filenamebb, 'w');
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_FILE, $fp);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	fclose($fp);
+}
+
+// Read first line of BED file to get CHR and start/end values
+$handle = fopen("http://api.godmc.org.uk/v0.1/dl/bed/cpg/".$query, "r");
+$line = fgets($handle);
+$pieces = explode("\t", $line);
+$chr = str_replace("chr","",$pieces[0]);
+$start = $pieces[1];
+$end = $pieces[2];
+
+
 ?>
 
 <!-- Main -->
@@ -17,31 +51,55 @@ if (isset($_GET['cpg']) && $_GET['cpg'] != '') {
 					
 				</header>
 				<div class="container">
-					<section>
-						
-
-            			<script language="javascript" src="//www.biodalliance.org/release-0.13/dalliance-compiled.js"></script>
+					<section>					
+						<script language="javascript" src="//www.biodalliance.org/release-0.13/dalliance-compiled.js"></script>
 						<script language="javascript">
-						  new Browser({
-						    chr:          '22',
-						    viewStart:    30700000,
-						    viewEnd:      30900000,
+						  new Browser({		   
 
-						    coordSystem: {
-						      speciesName: 'Human',
-						      taxon: 9606,
-						      auth: 'GRCh',
-						      version: '37',
-						      ucscName: 'hg19'
+						    chr:          '<?php echo $chr; ?>',
+						    viewStart:    <?php echo $start; ?>,
+						    //viewEnd:      <?php echo $end; ?>,
+						    viewEnd:      99999999,
+						    cookieKey:    'human-grc_h38',   
+
+							coordSystem: {
+						    	speciesName: 'Human',
+						    	taxon: 9606,
+						    	auth: 'GRCh',
+						    	version: '38',
+						    	ucscName: 'hg38',
 						    },
 
-						    sources:     [{name:                 'Genes',
-						                   desc:                 'Gene structures from GENCODE 19',
-						                   bwgURI:               'http://api.godmc.org.uk/v0.1/dl/bed/cpg/cg17242362',
-						                   stylesheet_uri:       '//www.biodalliance.org/stylesheets/gencode.xml',
-						                   collapseSuperGroups:  true,
-						                   trixURI:              '//www.biodalliance.org/datasets/geneIndex.ix'},
-						                  ],
+						    sources: [ 
+						    	
+						    	{	name:                 'Genome',
+									twoBitURI:            '//www.biodalliance.org/datasets/hg38.2bit',
+									tier_type:            'sequence'},
+
+								{	name:                 'Genes',
+									desc:                 'Gene structures from GENCODE 19',
+									bwgURI:               '//www.biodalliance.org/datasets/gencode.bb',
+									stylesheet_uri:       '//www.biodalliance.org/stylesheets/gencode.xml',
+									collapseSuperGroups:  true,
+									trixURI:              '//www.biodalliance.org/datasets/geneIndex.ix'},
+
+							   {	name:                 'Repeats',
+									desc:                 'Repeat annotation from Ensembl',
+									bwgURI:               '//www.biodalliance.org/datasets/repeats.bb',
+									stylesheet_uri:       '//www.biodalliance.org/stylesheets/bb-repeats.xml'},
+
+								{	name:                 'Conservation',
+									desc:                 'Conservation', 
+									bwgURI:               '//www.biodalliance.org/datasets/phastCons46way.bw',
+									noDownsample:         true},
+
+						    	{ 	name: '<?php echo $query; ?>',
+                   					bwgURI: '//<?php echo $hosturi; ?>/data/<?php echo $query; ?>.bb',
+									stylesheet_uri:  '//<?php echo $hosturi; ?>/scatter.xml',
+									collapseSuperGroups:  true,
+                   				},	
+                   			],
+						    
 
 						  });
 						</script>

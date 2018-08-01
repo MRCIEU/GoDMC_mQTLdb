ac993c59fb009fa80ec61f8e584d3db66db155bf
Paul Smith <paul@pjsweb.uk>
2018-08-01 09:35:27 -0700

    Removed redundant markup and added attribution links

--- footer.php
+++ footer.php
@@ -2,45 +2,12 @@
 		<!-- Footer -->
 			<footer id="footer">
 				<div class="container">
-					<div class="row double">
-						<div class="6u">
-							<div class="row collapse-at-2">
-								<div class="6u">
-									<h3>Accumsan</h3>
-									<ul class="alt">
-										<li><a href="#">Nascetur nunc varius</a></li>
-										<li><a href="#">Vis faucibus sed tempor</a></li>
-										<li><a href="#">Massa amet lobortis vel</a></li>
-										<li><a href="#">Nascetur nunc varius</a></li>
-									</ul>
-								</div>
-								<div class="6u">
-									<h3>Faucibus</h3>
-									<ul class="alt">
-										<li><a href="#">Nascetur nunc varius</a></li>
-										<li><a href="#">Vis faucibus sed tempor</a></li>
-										<li><a href="#">Massa amet lobortis vel</a></li>
-										<li><a href="#">Nascetur nunc varius</a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="6u">
-							<!-- <h2>GoDMC Website</h2> -->
-							<p>More information about GoDMC can be found on the <a href="http://godmc.org.uk/">project website</a></p>
-							<!-- <ul class="icons">
-								<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-								<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-								<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-								<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
-								<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
-							</ul> -->
-						</div>
-					</div>
 					<ul class="copyright">
-						<li>&copy; 2018. All rights reserved.</li>
-						<li>Website: <a href="http://templated.co">TEMPLATED</a> | <a href="http://pjsweb.uk">PJSWeb</a></li>
-						
+						<li>&copy; <?php echo date("Y"); ?>. All rights reserved.</li>
+						<li>Website: <a href="https://pjsweb.uk">PJS Web</a></li>
+						<li>Design: <a href="http://templated.co">TEMPLATED</a></li>
+						<li>Images: <a href="http://unsplash.com">Unsplash</a></li>
+						<li>Icons: <a href="https://www.flaticon.com/authors/smartline" title="Smartline">Smartline</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></li>
 					</ul>
 				</div>
 			</footer>
